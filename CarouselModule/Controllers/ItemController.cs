af622997e1fd2506b99cd0b7f2b3c9e449eae435
Kolos Mátyás Székely <kolos.szekely@stud.uni-corvinus.hu>
2023-05-22 21:36:34 +0200

    Add project files.

---
+++ CarouselModule/Controllers/ItemController.cs
@@ -0,0 +1,110 @@
+﻿/*
+' Copyright (c) 2023 Sebestyen Bala
+'  All rights reserved.
+' 
+' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+' TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+' THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+' CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+' DEALINGS IN THE SOFTWARE.
+' 
+*/
+
+using Carousel.Dnn.CarouselModule.Components;
+using Carousel.Dnn.CarouselModule.Models;
+using DotNetNuke.Entities.Users;
+using DotNetNuke.Framework.JavaScriptLibraries;
+using DotNetNuke.Services.FileSystem.Internal;
+using DotNetNuke.Web.Mvc.Framework.ActionFilters;
+using DotNetNuke.Web.Mvc.Framework.Controllers;
+using System;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Carousel.Dnn.CarouselModule.Controllers
+{
+    [DnnHandleError]
+    public class ItemController : DnnController
+    {
+
+        public void Delete(int itemId)
+        {
+            ItemManager.Instance.DeleteItem(itemId, ModuleContext.ModuleId);
+            return;
+        }
+
+        public ActionResult Edit(int itemId = -1)
+        {
+            DotNetNuke.Framework.JavaScriptLibraries.JavaScript.RequestRegistration(CommonJs.DnnPlugins);
+
+            var userlist = UserController.GetUsers(PortalSettings.PortalId);
+            var users = from user in userlist.Cast<UserInfo>().ToList()
+                        select new SelectListItem { Text = user.DisplayName, Value = user.UserID.ToString() };
+
+            ViewBag.Users = users;
+
+            var item = (itemId == -1)
+                 ? new Item { ModuleId = ModuleContext.ModuleId }
+                 : ItemManager.Instance.GetItem(itemId, ModuleContext.ModuleId);
+
+            return View(item);
+        }
+
+        [HttpPost]
+        [DotNetNuke.Web.Mvc.Framework.ActionFilters.ValidateAntiForgeryToken]
+        public ActionResult Edit(Item item)
+        {
+            if (item.ItemId == -1)
+            {
+                item.CreatedByUserId = User.UserID;
+                item.CreatedOnDate = DateTime.UtcNow;
+                item.LastModifiedByUserId = User.UserID;
+                item.LastModifiedOnDate = DateTime.UtcNow;
+
+                ItemManager.Instance.CreateItem(item);
+            }
+            else
+            {
+                var existingItem = ItemManager.Instance.GetItem(item.ItemId, item.ModuleId);
+                existingItem.LastModifiedByUserId = User.UserID;
+                existingItem.LastModifiedOnDate = DateTime.UtcNow;
+                existingItem.ItemName = item.ItemName;
+                existingItem.ItemTitle = item.ItemTitle;
+                existingItem.ItemAlt = item.ItemAlt;
+                existingItem.ItemLink = item.ItemLink;
+                existingItem.ItemImgPath = item.ItemImgPath;
+                existingItem.ItemDescription = item.ItemDescription;
+                existingItem.AssignedUserId = item.AssignedUserId;
+
+                ItemManager.Instance.UpdateItem(existingItem);
+            }
+
+            return RedirectToDefaultRoute();
+        }
+
+        [HttpPost]
+        public void UploadImage(HttpPostedFileBase image)
+        {
+            if (image == null)
+            {
+                return;
+            }
+            else
+            {
+                var fileName = Path.GetFileName(image.FileName);
+                var path = Path.Combine("C:\\inetpub\\wwwroot\\", fileName);
+                image.SaveAs(path);
+                return;
+            }
+        }
+
+        [ModuleAction(ControlKey = "Edit", TitleKey = "AddItem")]
+        public ActionResult Index()
+        {
+            var items = ItemManager.Instance.GetItems(ModuleContext.ModuleId);
+            return View(items);
+        }
+    }
+}
---
+++ CarouselModule/Controllers/SettingsController.cs
@@ -0,0 +1,56 @@
+﻿/*
+' Copyright (c) 2023 Sebestyen Bala
+'  All rights reserved.
+' 
+' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+' TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+' THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+' CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+' DEALINGS IN THE SOFTWARE.
+' 
+*/
+
+using Carousel.Dnn.CarouselModule.Components;
+using DotNetNuke.Collections;
+using DotNetNuke.Security;
+using DotNetNuke.Web.Mvc.Framework.ActionFilters;
+using DotNetNuke.Web.Mvc.Framework.Controllers;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace Carousel.Dnn.CarouselModule.Controllers
+{
+    [DnnModuleAuthorize(AccessLevel = SecurityAccessLevel.Edit)]
+    [DnnHandleError]
+    public class SettingsController : DnnController
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public ActionResult Settings()
+        {
+            var items = ItemManager.Instance.GetItems(ModuleContext.ModuleId);
+            return View(items);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="supportsTokens"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [ValidateInput(false)]
+        [DotNetNuke.Web.Mvc.Framework.ActionFilters.ValidateAntiForgeryToken]
+        public ActionResult Settings(Models.Settings settings)
+        {
+            ModuleContext.Configuration.ModuleSettings["CarouselModule_Setting1"] = settings.Setting1.ToString();
+            ModuleContext.Configuration.ModuleSettings["CarouselModule_Setting2"] = settings.Setting2.ToUniversalTime().ToString("u");
+
+            return RedirectToDefaultRoute();
+        }
+
+
+    }
+}
